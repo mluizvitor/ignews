726b43d23e03b6e2043b3fb36661183686af05ab
Vitor Monteiro <mluizvitor@gmail.com>
2022-03-12 17:25:44 -0300

    ✨ Added custom NextJS document with fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
+import Head from "next/head";
 import styles from "../styles/home.module.scss";
 
 export default function Home() {
-  return <h1 className={styles.title}>Hello Next.js</h1>;
+  return (
+    <>
+      <Head>
+        <title>Início · ig.news</title>
+      </Head>
+
+      <h1 className={styles.title}>Hello Next.js</h1>
+    </>
+  );
 }
