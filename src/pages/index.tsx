ac52d1f25d1ba8c81eadbab455fa4ae227944347
Vitor Monteiro <mluizvitor@gmail.com>
2022-03-15 13:12:38 -0300

    🚧 Added SubscribeButton component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import { SubscribeButton } from "../components/SubscribeButton";
 import styles from "./home.module.scss";
 
 export default function Home() {
@@ -22,6 +23,8 @@ export default function Home() {
             Get access to the publications <br />
             <span>for $9.90 month</span>
           </p>
+
+          <SubscribeButton />
         </section>
 
         <Image
