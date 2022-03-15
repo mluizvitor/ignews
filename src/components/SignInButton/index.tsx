c10383618a8cb42e95d82cc2153964c98f16998b
Vitor Monteiro <mluizvitor@gmail.com>
2022-03-15 11:27:46 -0300

    💄 Styles updated

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,19 @@
-import { FaGithub } from "react-icons/fa";
-import { FiX } from "react-icons/fi";
+import { FiGithub, FiX } from "react-icons/fi";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
   const isUserLoggedIn = true;
+
   return isUserLoggedIn ? (
     <button type="button" className={styles.signInButton}>
-      <FaGithub color="#04d361" />
-      Vitor Monteiro
+      <FiGithub color="#04d361" />
+      mluizvitor
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
     <button type="button" className={styles.signInButton}>
-      <FaGithub color="#eba417" />
+      <FiGithub color="#eba417" />
       Sign in with GitHub
     </button>
   );
