3ec4b41bd4f7f444830ece24a33a9dec35427553
Vitor Monteiro <mluizvitor@gmail.com>
2022-03-15 12:32:45 -0300

    🚧 Added content to Home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Image from "next/image";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -7,7 +9,28 @@ export default function Home() {
         <title>Início · ig.news</title>
       </Head>
 
-      <h1>Hello Next.js</h1>
+      <main className={styles.containerContent}>
+        <section className={styles.hero}>
+          <span>👏 Hey, wellcome!</span>
+
+          <h1>
+            News about <br />
+            the <span>React</span> world
+          </h1>
+
+          <p>
+            Get access to the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <Image
+          src="/images/avatar-m.svg"
+          alt="Girl coding"
+          height="520"
+          width="334"
+        />
+      </main>
     </>
   );
 }
