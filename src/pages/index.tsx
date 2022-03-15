5123231339737ee0f84bb832e28d758acfc7e421
Vitor Monteiro <mluizvitor@gmail.com>
2022-03-15 17:55:59 -0300

    🚧 Home build as SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { SubscribeButton } from "../components/SubscribeButton";
@@ -50,7 +50,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1KddLGARklXdX0VjjpuKzVxY");
 
   const product = {
@@ -66,5 +66,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
   return {
     props: { product },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
