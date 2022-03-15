b4bd562fa62ed347ea959df7efbf5d2f83936739
Vitor Monteiro <mluizvitor@gmail.com>
2022-03-15 17:27:35 -0300

    🚧 Added initial Stripe support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+    interval: string;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
-        <title>Início · ig.news</title>
+        <title>Home · ig.news</title>
       </Head>
 
       <main className={styles.containerContent}>
@@ -21,10 +31,12 @@ export default function Home() {
 
           <p>
             Get access to the publications <br />
-            <span>for $9.90 month</span>
+            <span>
+              for {product.amount} / {product.interval}
+            </span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <Image
@@ -37,3 +49,22 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1KddLGARklXdX0VjjpuKzVxY");
+
+  const product = {
+    priceId: price.id,
+
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+
+    interval: price.recurring.interval,
+  };
+
+  return {
+    props: { product },
+  };
+};
