dbf04fac9a5c643f512a054fae1fa8a342fb4f13
Vitor Monteiro <mluizvitor@gmail.com>
2022-03-19 17:56:28 -0300

    ✨ Added Github authentication

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,28 @@
-import { FiGithub, FiX } from "react-icons/fi";
+import Image from 'next/image';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import { FiGithub, FiX } from 'react-icons/fi';
 
-import styles from "./styles.module.scss";
+import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FiGithub color="#04d361" />
-      mluizvitor
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
       <FiGithub color="#eba417" />
       Sign in with GitHub
     </button>
