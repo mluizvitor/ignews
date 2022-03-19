58f2d8f4cb28b5c2185d5077fdef20fd97522b0f
Vitor Monteiro <mluizvitor@gmail.com>
2022-03-19 17:59:31 -0300

    🩹 Imported "version" as recommended

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,10 +1,12 @@
-import { version } from "../../package.json";
-import Stripe from "stripe";
+import AppPackage from '../../package.json';
+import Stripe from 'stripe';
+
+const { version } = AppPackage;
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
-  apiVersion: "2020-08-27",
+  apiVersion: '2020-08-27',
   appInfo: {
-    name: "Ignews",
+    name: 'Ignews',
     version,
   },
 });
