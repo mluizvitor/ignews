c2c5865d75d4d3d86c74883bfb892db7c6e609c5
Vitor Monteiro <mluizvitor@gmail.com>
2022-04-01 20:42:10 -0300

    🚧 Webhook receives Stripe update & delete events

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -23,7 +23,11 @@ export const config = {
   },
 };
 
-const relevantEvents = new Set(['checkout.session.completed']);
+const relevantEvents = new Set([
+  'checkout.session.completed',
+  'customer.subscription.updated',
+  'customer.subscription.deleted'
+]);
 
 export default async ( req: NextApiRequest, res: NextApiResponse ) => {
   if (req.method === 'POST') {
@@ -48,6 +52,18 @@ export default async ( req: NextApiRequest, res: NextApiResponse ) => {
     if (relevantEvents.has(type)) {
       try {
         switch (type) {
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+
+            const subscription = event.data.object as Stripe.Subscription;
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            );
+
+            break;
           case 'checkout.session.completed':
             const checkoutSession = event.data
               .object as Stripe.Checkout.Session;
@@ -55,6 +71,7 @@ export default async ( req: NextApiRequest, res: NextApiResponse ) => {
             await saveSubscription(
               checkoutSession.subscription.toString(),
               checkoutSession.customer.toString(),
+              true
             );
 
             break;
